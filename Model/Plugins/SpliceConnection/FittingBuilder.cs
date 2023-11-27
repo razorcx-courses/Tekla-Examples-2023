402b1c222d2a51f0248103bafb4c4f32574ece52
Courses Razor <courses@razorcx.com>
2023-11-27 06:41:14 -0800

    add plane builder and refactor fitting builder

--- Model/Plugins/SpliceConnection/FittingBuilder.cs
+++ Model/Plugins/SpliceConnection/FittingBuilder.cs
@@ -5,58 +5,87 @@ namespace SpliceConn
 {
     public class FittingBuilder
     {
+        private readonly PlaneBuilder _planeBuilder = new PlaneBuilder();
+
         //Creates a gap between the beams
         public bool CreateGapBetweenBeams(Beam primaryBeam, Beam secondaryBeam, double gap)
         {
             if (primaryBeam == null || secondaryBeam == null) return false;
 
+            //find gap vector distances
             var primaryBeamGapVector = new Vector(primaryBeam.EndPoint - primaryBeam.StartPoint);
             primaryBeamGapVector.Normalize(gap);
 
             var secondaryBeamGapVector = new Vector(secondaryBeam.EndPoint - secondaryBeam.StartPoint);
             secondaryBeamGapVector.Normalize(gap);
 
-            //if (primaryBeam.StartPoint == secondaryBeam.StartPoint)
+            //reverse gap vectors depending on direction of beams relative to each other
+            ReverseGapVector(primaryBeam, secondaryBeam, ref primaryBeamGapVector, ref secondaryBeamGapVector);
+
+            //find start and end points for gap(fittings)
+            //default - if (primaryBeam.StartPoint == secondaryBeam.StartPoint)
             var primaryBeamEndPoint = primaryBeam.StartPoint;
             var secondaryBeamEndPoint = secondaryBeam.StartPoint;
 
-            if (primaryBeam.EndPoint == secondaryBeam.StartPoint)
-            {
-                primaryBeamGapVector *= -1; //reverse vector
+            //adjust points depending on direction of beams relative to each other
+            AdjustStartAndEndPoints(primaryBeam, secondaryBeam, ref primaryBeamEndPoint, ref secondaryBeamEndPoint);
 
-                primaryBeamEndPoint = primaryBeam.EndPoint;
-                secondaryBeamEndPoint = secondaryBeam.StartPoint;
-            }
-
-            if (primaryBeam.StartPoint == secondaryBeam.EndPoint)
-            {
-                secondaryBeamGapVector *= -1;
-
-                primaryBeamEndPoint = primaryBeam.StartPoint;
-                secondaryBeamEndPoint = secondaryBeam.EndPoint;
-            }
-
-            if (primaryBeam.EndPoint == secondaryBeam.EndPoint)
-            {
-                primaryBeamGapVector *= -1;
-                secondaryBeamGapVector *= -1;
-
-                primaryBeamEndPoint = primaryBeam.EndPoint;
-                secondaryBeamEndPoint = secondaryBeam.EndPoint;
-            }
-
+            //create fittings
             var primaryFittingResult = CreateFitting(primaryBeam, primaryBeamEndPoint, primaryBeamGapVector);
-
             var secondaryFittingResult = CreateFitting(secondaryBeam, secondaryBeamEndPoint, secondaryBeamGapVector);
 
             return primaryFittingResult && secondaryFittingResult;
 
         }
 
-        //Creates the fittings used to create the gap between the beams
-        private static bool CreateFitting(Beam beam, Point beamEndPoint, Vector beamGapVector)
+        private static void ReverseGapVector(Beam primaryBeam, Beam secondaryBeam, ref Vector primaryBeamGapVector,
+            ref Vector secondaryBeamGapVector)
         {
-            var plane = GetPlaneForFitting(beam, beamEndPoint, beamGapVector);
+            if (primaryBeam.EndPoint == secondaryBeam.StartPoint)
+            {
+                primaryBeamGapVector *= -1; //reverse vector
+            }
+
+            if (primaryBeam.StartPoint == secondaryBeam.EndPoint)
+            {
+                secondaryBeamGapVector *= -1;
+            }
+
+            if (primaryBeam.EndPoint == secondaryBeam.EndPoint)
+            {
+                primaryBeamGapVector *= -1;
+                secondaryBeamGapVector *= -1;
+            }
+        }
+
+        private static void AdjustStartAndEndPoints(Beam primaryBeam, Beam secondaryBeam, ref Point primaryBeamEndPoint,
+            ref Point secondaryBeamEndPoint)
+        {
+            if (primaryBeam.EndPoint == secondaryBeam.StartPoint)
+            {
+                primaryBeamEndPoint = primaryBeam.EndPoint;
+                secondaryBeamEndPoint = secondaryBeam.StartPoint;
+            }
+
+            if (primaryBeam.StartPoint == secondaryBeam.EndPoint)
+            {
+                primaryBeamEndPoint = primaryBeam.StartPoint;
+                secondaryBeamEndPoint = secondaryBeam.EndPoint;
+            }
+
+            if (primaryBeam.EndPoint == secondaryBeam.EndPoint)
+            {
+                primaryBeamEndPoint = primaryBeam.EndPoint;
+                secondaryBeamEndPoint = secondaryBeam.EndPoint;
+            }
+        }
+
+        //Creates the fittings used to create the gap between the beams
+        private bool CreateFitting(Beam beam, Point beamEndPoint, Vector beamGapVector)
+        {
+            var coordinateSystem = beam.GetCoordinateSystem();
+
+            var plane = _planeBuilder.GetPlaneForFitting(coordinateSystem, beamEndPoint, beamGapVector);
 
             var fitting = new Fitting
             {
@@ -65,49 +94,6 @@ namespace SpliceConn
             };
 
             return fitting.Insert();
-        }
-
-        private static Plane GetPlaneForFitting(Beam beam, Point beamEndPoint, Vector beamGapVector)
-        {
-            var coordinateSystem = beam.GetCoordinateSystem();
-
-            var originPoint = GetOriginPointForFitting(beamEndPoint, beamGapVector);
-
-            var axisX = GetAxisXForFitting(coordinateSystem);
-
-            var plane = GetPlane(originPoint, axisX, coordinateSystem);
-            return plane;
-        }
-
-        private static Point GetOriginPointForFitting(Point beamEdge, Vector beamVector)
-        {
-            var originPoint = new Point(beamEdge);
-            originPoint.Translate(beamVector.X, beamVector.Y, beamVector.Z);
-            return originPoint;
-        }
-
-        private static Vector GetAxisXForFitting(CoordinateSystem coordinateSystem)
-        {
-            var axisX = new Vector(Vector.Cross(coordinateSystem.AxisX,
-                Vector.Cross(coordinateSystem.AxisX, coordinateSystem.AxisY)));
-            return axisX;
-        }
-
-        private static Plane GetPlane(Point beamEdge, Vector axisX, CoordinateSystem coordinateSystem)
-        {
-            var length = 500;
-
-            var plane = new Plane
-            {
-                Origin = new Point(beamEdge),
-                AxisX = axisX
-            };
-
-            plane.AxisX.Normalize(length);
-
-            plane.AxisY = new Vector(Vector.Cross(coordinateSystem.AxisX, coordinateSystem.AxisY));
-            plane.AxisY.Normalize(length);
-            return plane;
         }
     }
 }
---
+++ Model/Plugins/SpliceConnection/PlaneBuilder.cs
@@ -0,0 +1,49 @@
+﻿using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+namespace SpliceConn
+{
+    public class PlaneBuilder
+    {
+        private const double PlaneLength = 500;
+
+        public Plane GetPlaneForFitting(CoordinateSystem coordinateSystem, Point point, Vector distanceVector)
+        {
+            var originPoint = GetOriginPointForFitting(point, distanceVector);
+
+            var axisX = GetAxisXForFitting(coordinateSystem);
+
+            var plane = GetPlane(originPoint, axisX, coordinateSystem);
+            return plane;
+        }
+
+        private static Point GetOriginPointForFitting(Point point, Vector distanceVector)
+        {
+            var originPoint = new Point(point);
+            originPoint.Translate(distanceVector.X, distanceVector.Y, distanceVector.Z);
+            return originPoint;
+        }
+
+        private static Vector GetAxisXForFitting(CoordinateSystem coordinateSystem)
+        {
+            var axisX = new Vector(Vector.Cross(coordinateSystem.AxisX,
+                Vector.Cross(coordinateSystem.AxisX, coordinateSystem.AxisY)));
+            return axisX;
+        }
+
+        private static Plane GetPlane(Point point, Vector axisX, CoordinateSystem coordinateSystem)
+        {
+            var plane = new Plane
+            {
+                Origin = new Point(point),
+                AxisX = axisX
+            };
+
+            plane.AxisX.Normalize(PlaneLength);
+
+            plane.AxisY = new Vector(Vector.Cross(coordinateSystem.AxisX, coordinateSystem.AxisY));
+            plane.AxisY.Normalize(PlaneLength);
+            return plane;
+        }
+    }
+}
